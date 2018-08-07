78fce6a2cf6fb19b33392402968dee973708cc01
Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
2018-08-07 01:47:17 -0400

    WIP

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 // DB config
 const db = require('./config/keys').mongoURI;
@@ -13,6 +23,11 @@ mongoose
 .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello!!'));
+
+// User routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
