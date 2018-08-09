db320ab8a3bd5ea2d2d67355c96e7ce90984afa9
Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
2018-08-08 23:01:53 -0400

    Added Dev URL

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://admin:admin12@ds115022.mlab.com:15022/mernstackapp'
+  mongoURI: 'mongodb://admin:admin12@ds115022.mlab.com:15022/mernstackapp',
+  mongoURIDev: 'mongodb://localhost:27017/mernstackapp'
 }
