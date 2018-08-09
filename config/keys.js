3f06729018514f21273c86fb12a24bf85192b9d3
Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
2018-08-09 03:07:13 -0400

    WIP

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI: 'mongodb://admin:admin12@ds115022.mlab.com:15022/mernstackapp',
-  mongoURIDev: 'mongodb://localhost:27017/mernstackapp'
+  mongoURIDev: 'mongodb://localhost:27017/mernstackapp',
+  secretOrKey: 'secret'
 }
